1ea912d2f8775791a26a5911055ad0452fcc7609
Origami404 <Origami404@foxmail.com>
2023-11-01 21:21:33 +0800

    [matmul] adjust thread loop order

--- matmul.cu
+++ matmul.cu
@@ -5,23 +5,50 @@
 #include <iostream>
 
 constexpr size_t N = 8192;
+// how many elements a thread handles (THD_N * THD_N)
+auto constexpr THD_N = 4;
+// how many threads in a block
+auto constexpr BLK_N = 8;
 
 static inline bool feq(float a, float b) { return abs(a - b) <= 1e-5f; }
 // use macro for both host and device
 #define at(arr, i, j) ((arr)[(i)*N + (j)])
 
 // Kernel function to add the elements of two arrays
-__global__ void matmul(float *C, float *A, float *B, size_t const THD_N) {
-  auto const tx_beg = (blockIdx.x * blockDim.x + threadIdx.x) * THD_N;
-  auto const ty_beg = (blockIdx.y * blockDim.y + threadIdx.y) * THD_N;
+__global__ void matmul(float *C, float *A, float *B) {
+  auto const tx_beg = (blockIdx.x * BLK_N + threadIdx.x) * THD_N;
+  auto const ty_beg = (blockIdx.y * BLK_N + threadIdx.y) * THD_N;
 
-  for (auto tx = tx_beg; tx < tx_beg + THD_N; tx++) {
-    for (auto ty = ty_beg; ty < ty_beg + THD_N; ty++) {
-      auto sum = 0.0f;
-      for (auto k = 0; k < N; k++) {
-        sum += at(A, tx, k) * at(B, k, ty);
+  float sC[THD_N][THD_N], sA[THD_N], sB[THD_N];
+
+  // set sC to 0
+  for (auto i = 0; i < THD_N; i++) {
+    for (auto j = 0; j < THD_N; j++) {
+      sC[i][j] = 0.0f;
+    }
+  }
+
+  for (auto k = 0; k < N; k++) {
+    // load sA and sB
+    for (auto i = 0; i < THD_N; i++) {
+      sA[i] = at(A, tx_beg + i, k);
+    }
+    for (auto j = 0; j < THD_N; j++) {
+      sB[j] = at(B, k, ty_beg + j);
+    }
+
+    // compute sC
+    for (auto i = 0; i < THD_N; i++) {
+      for (auto j = 0; j < THD_N; j++) {
+        sC[i][j] += sA[i] * sB[j];
       }
-      at(C, tx, ty) = sum;
+    }
+  }
+
+  // write back to C
+  for (auto i = 0; i < THD_N; i++) {
+    for (auto j = 0; j < THD_N; j++) {
+      at(C, tx_beg + i, ty_beg + j) = sC[i][j];
     }
   }
 }
@@ -54,16 +81,13 @@ int main(void) {
 
   cudaMemcpy(A, hA, MAT_SIZE, cudaMemcpyHostToDevice);
   cudaMemcpy(B, hB, MAT_SIZE, cudaMemcpyHostToDevice);
+  cudaMemset(C, 1, MAT_SIZE);
 
-  // how many elements a thread handles (THD_N * THD_N)
-  auto constexpr THD_N = 4;
-  // how many threads in a block
-  auto constexpr BLK_N = 8;
   dim3 constexpr blockDim{BLK_N, BLK_N, 1};
   dim3 constexpr gridDim{N / BLK_N / THD_N, N / BLK_N / THD_N, 1};
 
   auto const matmul_begin = std::chrono::steady_clock::now();
-  matmul<<<gridDim, blockDim>>>(C, A, B, THD_N);
+  matmul<<<gridDim, blockDim>>>(C, A, B);
 
   // Wait for GPU to finish before accessing on host
   cudaDeviceSynchronize();
